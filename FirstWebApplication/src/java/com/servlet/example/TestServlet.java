513b157465a0731d9465c5c3c398da54b2ea30e0
Devastator1979 <devastator1979@mail.ru>
2019-11-26 21:21:56 +0300

    First web app

--- FirstWebApplication/src/java/com/servlet/example/TestServlet.java
+++ FirstWebApplication/src/java/com/servlet/example/TestServlet.java
@@ -8,6 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TestServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+                processRecuest(request, response);
+        }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+            processRequest(request, response);
+        }
 
+    private void processRecuest(HttpServletRequest request, HttpServletResponse response) {
+        throw ServletException, IOException{
+            response.setContentType("text")
+        }
+    }
 
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
+
