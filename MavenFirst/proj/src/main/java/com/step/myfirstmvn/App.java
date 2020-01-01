04d55d22b63b8e4c2720131595064979c5f69e30
devastator1979 <brestdevastator@gmail.com>
2020-01-02 02:52:38 +0300

    123

---
+++ MavenFirst/proj/src/main/java/com/step/myfirstmvn/App.java
@@ -0,0 +1,13 @@
+package com.step.myfirstmvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
